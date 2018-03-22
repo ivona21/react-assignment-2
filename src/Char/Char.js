38d7e26ce014b531672525aa7502a4da0229a817
Ivona Malekovic <ivonamalekovic@hotmail.com>
2018-03-22 13:14:04 +0100

    4

---
+++ src/Char/Char.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Char = () => {
+    const charStyle = {
+        display: "inline-block", 
+        padding: "16px", 
+        textAlign: "center", 
+        margin: "16px", 
+        border: "1px solid black"
+    }
+
+    return (<div style={charStyle}></div>)
+}
+
+export default Char;
